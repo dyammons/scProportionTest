61b6ff1a41d9c2b7625d46655e65af6b1c6735be
Sam A Miller <62508068+SamuelAMiller1@users.noreply.github.com>
2021-03-26 13:00:06 -0400

    Update permutation_coord_plot.R

--- R/permutation_coord_plot.R
+++ R/permutation_coord_plot.R
@@ -1,3 +1,20 @@
+#' Plot Permutation Results
+#'
+#' @importFrom forcats fct_reorder
+#' @importFrom dplyr desc case_when
+#' @importFrom ggplot2 ggplot aes geom_bar coord_polar
+#' coord_flip scale_color_manual
+#'
+#' @param sc_utils_obj sc_utils object
+#' @param FDR_threshold FDR value cutoff for significance
+#' @param log2FD_threshold Absolute value of log2FD cutoff for significance
+#' @param order_clusters Whether to order the clusters by observed log2FD
+#'
+#' @rdname permutation_coord_plot-function
+#'
+#' @export
+
+
 permutation_coord_plot <- function(
 	sc_utils_obj,
 	FDR_threshold = 0.05,
